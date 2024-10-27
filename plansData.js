534af80738fc9cd57238ec1af4ac24304924f672
Dan Le <didan.mobe@gmail.com>
2024-10-27 16:34:10 +0700

    Add

---
+++ plansData.js
@@ -0,0 +1,142 @@
+const plansData = [
+  {
+    sectionTitle: "Thông tin chung",
+    features: [
+      {
+        title: "Đơn vị tin lưu trữ",
+        description: ["500 storage units"],
+        values: { basic: "500", standard: "1,000", premium: "5,000" },
+      },
+      {
+        title: "Trang bố cục",
+        description: [
+          "Tổng số lượng dữ liệu tin tức",
+          "Sản phẩm, dịch vụ",
+          "Ti tuyển dụng",
+        ],
+        values: { basic: "25", standard: "50", premium: "100" },
+      },
+      {
+        title: "Dung lượng lưu trữ",
+        description: [
+          "Tổng số lượng dữ liệu tin tức",
+          "Sản phẩm, dịch vụ",
+          "Ti tuyển dụng",
+        ],
+        values: { basic: "5 GB", standard: "10 GB", premium: "50 GB" },
+      },
+      {
+        title: "Băng thông",
+        description: [
+          "Tổng số lượng dữ liệu tin tức",
+          "Sản phẩm, dịch vụ",
+          "Ti tuyển dụng",
+        ],
+        values: { basic: "20 GB", standard: "50 GB", premium: "Unlimited" },
+      },
+      {
+        title: "Mẫu giao diện",
+        description: [
+          "Tổng số lượng dữ liệu tin tức",
+          "Sản phẩm, dịch vụ",
+          "Ti tuyển dụng",
+        ],
+        values: { basic: "Basic", standard: "Standard", premium: "Premium" },
+      },
+      {
+        title: "Ngôn ngữ",
+        description: [
+          "Tổng số lượng dữ liệu tin tức",
+          "Sản phẩm, dịch vụ",
+          "Ti tuyển dụng",
+        ],
+        values: { basic: "1", standard: "1", premium: "2" },
+      },
+      {
+        title: "Công cụ quản trị website",
+        description: [
+          "Tổng số lượng dữ liệu tin tức",
+          "Sản phẩm, dịch vụ",
+          "Ti tuyển dụng",
+        ],
+        values: {
+          basic: '<i class="fa-solid fa-check"></i>',
+          standard: '<i class="fa-solid fa-check"></i>',
+          premium: '<i class="fa-solid fa-check"></i>',
+        },
+      },
+      {
+        title: "Chứng chỉ bảo mật SSL",
+        description: [
+          "Tổng số lượng dữ liệu tin tức",
+          "Sản phẩm, dịch vụ",
+          "Ti tuyển dụng",
+        ],
+        values: {
+          basic: '<i class="fa-solid fa-check"></i>',
+          standard: '<i class="fa-solid fa-check"></i>',
+          premium: '<i class="fa-solid fa-check"></i>',
+        },
+      },
+      {
+        title: "Thông báo qua Email",
+        description: [
+          "Gói website đã bao gồm công cụ gửi email với giới hạn gửi email trên 1 tháng.",
+          "Gửi email các thông tin như xác nhận đơn hàng, thông tin đăng ký,… từ website tới khách hàng qua tên địa chỉ email sender@mailer.matbaows.com",
+        ],
+        values: {
+          basic: "100 lần",
+          standard: "200 lần",
+          premium: "500 lần",
+        },
+      },
+      {
+        title: "Nút liên hệ nhanh",
+        description: [
+          "Nút liên hệ nhanh qua: Điện thoại, Messenger, Zalo, Telegram, WhatsApp, SMS, …giúp bạn tương tác với khách hàng dễ dàng hơn.",
+        ],
+        values: {
+          basic: '<i class="fa-solid fa-check"></i>',
+          standard: '<i class="fa-solid fa-check"></i>',
+          premium: '<i class="fa-solid fa-check"></i>',
+        },
+      },
+      {
+        title: "Phát triển tính năng",
+        description: [
+          "Phát triển thêm tính năng theo yêu cầu của bạn, đơn giá dựa theo khối lượng công việc lập trình tính năng.",
+          "Tuy nhiên, bạn có thể mua thêm tính năng đóng gói ở <a href='https://example.com' target='_blank'>kho ứng dụng web.</a> ",
+        ],
+        values: {
+          basic: "-",
+          standard: "-",
+          premium: '<i class="fa-solid fa-check"></i>',
+        },
+      },
+      {
+        title: "Bỏ bản quyền Mắt Bão WS",
+        description: ["Xóa bản quyền Mắt Bão WS dưới chân trang."],
+        values: {
+          basic: "-",
+          standard: "-",
+          premium: '<i class="fa-solid fa-check"></i>',
+        },
+      },
+    ],
+  },
+  //   SECTION 2
+  {
+    sectionTitle: "Quản lý bán hàng",
+    features: [
+      {
+        title: "Bỏ bản quyền Mắt Bão WS",
+        description: ["Xóa bản quyền Mắt Bão WS dưới chân trang."],
+        values: {
+          basic: "-",
+          standard: "-",
+          premium: '<i class="fa-solid fa-check"></i>',
+        },
+      },
+    ],
+  },
+];
